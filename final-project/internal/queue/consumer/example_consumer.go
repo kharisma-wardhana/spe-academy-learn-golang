3e936f7793955d70b8153c4cd9dc6a04ba7d4f64
Kharisma Wardhana <dev.kharis29@gmail.com>
2025-07-13 20:12:13 +0700

    feat: add final-project folder

---
+++ final-project/internal/queue/consumer/example_consumer.go
@@ -0,0 +1,34 @@
+package consumer
+
+import (
+	"context"
+
+	"github.com/kharisma-wardhana/spe-academy-learn-golang/final-project/entity"
+	"github.com/kharisma-wardhana/spe-academy-learn-golang/final-project/internal/helper"
+	mongoRepo "github.com/kharisma-wardhana/spe-academy-learn-golang/final-project/internal/repository/mongodb"
+)
+
+type ExampleQueue struct {
+	ctx          context.Context
+	logMongoRepo mongoRepo.LogRepository
+}
+
+type ExampleConsumer interface {
+	Process(payload map[string]interface{}) error
+}
+
+func NewExampleConsumer(
+	ctx context.Context,
+	logMongoRepo mongoRepo.LogRepository,
+) ExampleConsumer {
+	return &ExampleQueue{ctx, logMongoRepo}
+}
+
+func (l *ExampleQueue) Process(payload map[string]interface{}) error {
+	var params entity.Log
+	params.LoadFromMap(payload)
+
+	helper.Dump(params)
+
+	return nil
+}
